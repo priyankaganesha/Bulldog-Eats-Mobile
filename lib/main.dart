4d1e8acaa99e788136df684a6dba869684190620
marcellahuang <huang_marcella@yahoo.com>
2020-09-07 21:06:58 -0700

    Sign-in interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
-import 'package:bulldog_eats/pages/HomePage.dart';
+import 'package:bulldog_eats/authenticate/models/User.dart';
+import 'package:bulldog_eats/services/Auth.dart';
 import 'package:flutter/material.dart';
+import 'package:bulldog_eats/authenticate/Wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  //initializes the firebase app
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -9,13 +16,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BC Eats',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    // StreamProvider listens to the stream and wraps the material app
+    // meaning anything wrapped within it can have access to the data provided by the stream
+    return StreamProvider<TheUser>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        title: 'Bulldogs Eats',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Wrapper(),
       ),
-      home: HomePage(),
     );
   }
 }
--- lib/pages/Settings.dart
+++ lib/pages/Settings.dart
@@ -1,3 +1,4 @@
+import 'package:bulldog_eats/services/Auth.dart';
 import 'package:flutter/material.dart';
 
 class Settings extends StatefulWidget {
@@ -8,12 +9,22 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final AuthService _auth = AuthService(); // method for AuthService
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
-        backgroundColor: Color(0xFF9dd2dc),
+        elevation: 0.0, // The Sign Out Button
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Logout'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ],
       ),
     );
   }
